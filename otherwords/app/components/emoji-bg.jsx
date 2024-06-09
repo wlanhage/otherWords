5f8ff4560ab4fdd384df4f5b3d1d5b7e25c39dda
wlanhage <wlanhage@gmail.com>
2024-06-09 17:33:26 +0200

    background video bug fix

--- otherwords/app/components/emoji-bg.jsx
+++ otherwords/app/components/emoji-bg.jsx
@@ -1,18 +1,19 @@
 function Background ({ children }) {
-    return (
-      <div className="h-screen w-screen relative">
-        <video
-          src="/emoji-bg.mp4"
-          autoPlay
-          loop
-          muted
-          style={{position: "absolute", width: "100%", left: "50%", top: "50%", height: "100%", objectFit: "cover", transform: "translate(-50%, -50%)", zIndex: "-1"}}
-        />
-        <div style={{position: "absolute"}}>
-          {children}
-        </div>
+  return (
+    <div className="h-screen w-screen relative">
+      <video
+        src="/emoji-bg.mp4"
+        autoPlay
+        loop
+        muted
+        style={{position: "absolute", width: "100%", left: "50%", top: "50%", height: "100%", objectFit: "cover", transform: "translate(-50%, -50%)", zIndex: "-1"}}
+      />
+      <div style={{position: "absolute", width: "100%", height: "100%", top: "0", left: "0", zIndex: "1"}}></div>
+      <div style={{position: "absolute"}}>
+        {children}
       </div>
-    );
-  }
+    </div>
+  );
+}
   
   export default Background;
